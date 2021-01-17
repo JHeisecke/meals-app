05beb7832c95773456e4da4a6946c89f02674891
Javier Heisecke <javierheiseckee@gmail.com>
2021-01-17 19:01:06 -0300

    Navegamos a pantalla de comidas por categoria

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,31 +1,48 @@
 import 'package:flutter/material.dart';
 
+import './category_meals_screen.dart';
+
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
   CategoryItem(this.title, this.color);
 
+  void _categorySelected(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) => CategoryMealsScreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15), //when widget reruns,
-      // flutter won't rebuilt padding because with const
-      // detects that it hasn't changed
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.headline6,
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.topRight,
+    return InkWell(
+      onTap: () {
+        _categorySelected(context);
+      },
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15), //when widget reruns,
+        // flutter won't rebuilt padding because with const
+        // detects that it hasn't changed
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
         ),
-        borderRadius: BorderRadius.circular(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.topRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
