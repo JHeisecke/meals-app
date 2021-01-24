fdd9913cc56774d9ed9e10a586238d222a43c8c3
Javier Heisecke <javierheiseckee@gmail.com>
2021-01-24 19:46:37 -0300

    Agrega appbar y scaffold al la pantallad de filtros

--- lib/widgets/screens/filter_screen.dart
+++ lib/widgets/screens/filter_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../utils/main_drawer.dart';
+
 class FilterScreen extends StatelessWidget {
   static const routeName = '/filter';
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Filters'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Filters"),
+      ),
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text('Filters'),
+      ),
     );
   }
 }
